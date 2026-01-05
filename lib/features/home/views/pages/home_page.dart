f87fbae84f88e4add5cb948775cc3292312ce8d3
AbdulteefRashid <abdulte97@gmail.com>
2026-01-06 00:37:57 +0300

    complete AppBar &Drawer

--- lib/features/home/views/pages/home_page.dart
+++ lib/features/home/views/pages/home_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/views/widgets/app_bar_button.dart';
+import 'package:news_app/core/views/widgets/app_drawer.dart';
 import 'package:news_app/features/home/cubit/home_cubit.dart';
 import 'package:news_app/features/home/views/widgets/custom_carousel_slider.dart';
 import 'package:news_app/features/home/views/widgets/recommendation_list_widget.dart';
 import 'package:news_app/features/home/views/widgets/title_headline_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -18,7 +26,33 @@ class HomePage extends StatelessWidget {
         return homeCubit;
       },
       child: Scaffold(
-        appBar: AppBar(title: const Text('Home Page')),
+        key: _scaffoldKey,
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AppBarButton(
+              iconData: Icons.menu,
+              onTap: () {
+                _scaffoldKey.currentState!.openDrawer();
+              },
+            ),
+          ),
+          actions: [
+            AppBarButton(
+              iconData: Icons.search,
+              onTap: () {},
+              hasPaddingBetween: true,
+            ),
+            const SizedBox(width: 8),
+            AppBarButton(
+              iconData: Icons.notifications_none_rounded,
+              onTap: () {},
+              hasPaddingBetween: true,
+            ),
+            const SizedBox(width: 12),
+          ],
+        ),
+        drawer: AppDrawer(),
         body: SafeArea(
           child: Builder(
             builder: (context) {
